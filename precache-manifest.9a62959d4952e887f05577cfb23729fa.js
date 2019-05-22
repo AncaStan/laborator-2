64d104dff24b2e8bdba1a640081dc9354fd132cb
AncaStan <ancagabriela.stan31@gmail.com>
2019-05-23 00:57:41 +0300

    Updates

---
+++ precache-manifest.9a62959d4952e887f05577cfb23729fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd6c9e9e51180d449f862d819f0f01b2",
+    "url": "/laborator-2-eHealth/index.html"
+  },
+  {
+    "revision": "5f25ebea5ea5b517141a",
+    "url": "/laborator-2-eHealth/static/css/main.e6ab37d1.chunk.css"
+  },
+  {
+    "revision": "049c60047fcc3cd6345c",
+    "url": "/laborator-2-eHealth/static/js/2.1cdd753e.chunk.js"
+  },
+  {
+    "revision": "5f25ebea5ea5b517141a",
+    "url": "/laborator-2-eHealth/static/js/main.af7c84d3.chunk.js"
+  },
+  {
+    "revision": "4c9e37d1efa9486eba33",
+    "url": "/laborator-2-eHealth/static/js/runtime~main.0d626782.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/laborator-2-eHealth/precache-manifest.9a62959d4952e887f05577cfb23729fa.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/laborator-2-eHealth/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
